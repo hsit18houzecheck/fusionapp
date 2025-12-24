7b0fd2bdb4d49dd38cb15f2899171340a4174dcf
Harpreet Singh <harpreet.singh@houzecheck.com>
2025-12-24 22:44:56 +0530

    project setup

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import BuilderDevTools from "@builder.io/dev-tools/next";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = BuilderDevTools()(
+  BuilderDevTools()({
+    /* config options here */
+    reactCompiler: true,
+    turbopack: {},
+  }),
+);
+
+export default nextConfig;
